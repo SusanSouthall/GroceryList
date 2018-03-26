9fbd0e839a14c62e86f2f68d7ca88b6b88fd954e
julia sheremet and Susan Southall <susan@>
2018-03-26 16:50:16 -0700

    create succsful looping array

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,26 @@ $(document).ready(function(){
 
   var blanks = ["item1", "item2", "item3", "item4", "item5", "item6"];
 
+
     blanks.forEach(function(blank){
       var finalItems = $("input#" + blank).val();
       $("ul#output").append("<li>" + finalItems + "</li>");
-
-      // $("#button").click(function(){
-      // $(this).toggleClass("toBuyList");
-      // });
     });
+    // $("form#toBuy").reset();
+    var lastItem = $("input#addItem").val();
+    // alert(lastItem);
+
+
+    // blanks.push(lastItem);
+    // $("ul#output").append("<li>" + lastItem + "</li>");
+    $("ul#output").append("<li>" + lastItem + "</li>")
+      //  finalItems.push(lastItem);
+      // $("ul#output").push("<li>" + lastItem + "</li>");
+      // $(".addList").append(lastItem);
+      debugger;
+      // for(let i =0; i < blanks.length; i++){
+      //   $("ul#output").append("<li>" + blanks[i] + "</li>");
+      // }
+
   });
 });
